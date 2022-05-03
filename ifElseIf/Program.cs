dac97205e88c6436536605b5952cf3e960aefca6
Ozgen Koklu <ozgen.koklu@gmail.com>
2022-05-03 18:47:51 +0300

    if else if ternary ödevi eklendi

---
+++ ifElseIf/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ifElseIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time>=6 && time<11){
+                Console.WriteLine("Günaydın!");
+              }else if(time <= 18)
+            {
+                Console.WriteLine("Iyı Gunler!");
+            }
+            else
+            {
+                Console.WriteLine("Iyı geceler!");
+            }
+            string sonuc = time <= 18 ? "Iyı Günler" : "Iyı geceler";
+
+            sonuc = time >= 6 && time < 11 ? "Günaydın!" : time <= 18 ? "Iyı Gunler!" : "Iyı Geceler!";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
