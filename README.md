d46c022ee8b0e51fa0ba04f5651a936fe18f08a0
Ozgen Koklu <ozgen.koklu@gmail.com>
2022-05-04 10:47:44 +0300

    while foreach ödevi eklendi

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# PATİKA DEV C# 101 dersi için oluşturduğum toplu repo'dur
+# PatikaDev C# 101 dersi için oluşturduğum toplu repo'dur
 
 Ders linki: https://app.patika.dev/courses/csharp-101
 
 Patika dev profilim: https://app.patika.dev/ozgenkoklu
+
+Derslerde ilerlemek için hackerrank soruları da çözmek gerekiyor. HackerRank profilim: https://www.hackerrank.com/ozgen_koklu
---
+++ whileForEach/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace whileForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1den başlayarak consoledan girilen sayıya kadar (sayı dahil) ortalama hesaplayan consolea yazıran program
+            Console.WriteLine("Lütfen bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+
+            }
+            Console.WriteLine(toplam / sayi);
+
+            //a'dan zye kadar tüm harfleri consolea yazdır.
+
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+
+            Console.WriteLine("*****FOREACH*****");
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
