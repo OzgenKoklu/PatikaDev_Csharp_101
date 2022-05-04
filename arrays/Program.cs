349f9801fbdde71f76cd44f58fde853f542b7c59
Ozgen Koklu <ozgen.koklu@gmail.com>
2022-05-04 11:46:25 +0300

    Array ödevi eklendi

---
+++ arrays/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //dizi taımlama
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //dizilere değer atama ve erişim
+            renkler[0] = "mavi";
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            //Döngülerle Dizi kullanımı
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());  
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.WriteLine("Lütfen {0}, sayısı giriniz", i + 1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine()); 
+
+            }
+
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+            Console.WriteLine("Ortalama: " + toplam / diziUzunlugu);
+        }
+    }
+}
