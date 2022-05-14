1d030d2b6682afe234cf62be9d78cf9474520c00
Ozgen Koklu <ozgen.koklu@gmail.com>
2022-05-14 17:40:37 +0300

    class notları eklendi

---
+++ classConsept/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace classConsept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Söz Dizini
+            //Class sinif adı
+            //Class SinifAdi
+            //{
+            //  [Erişim belirliyici] [veri tipi] ÖzellikAdi
+            //  [Erişim belirleyici] [geri dönüş tipi] MetodADı(parametre listesi)
+            // {
+            //  // metod komutları
+            // }
+            //}
+
+            //erişim belirleyiciler
+            // * Public
+            // * Private
+            // * Internal 
+            // * Protected 
+
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad = "Ayşe";
+            calisan1.Soyad = "Kara";
+            calisan1.No = 12514;
+            calisan1.Departman= "Insan kaynakları";
+
+            calisan1.CalisanBilgileri();
+
+            Console.WriteLine("*********");
+
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Deniz";
+            calisan2.Soyad = "Aslan";
+            calisan2.No = 25612;
+            calisan2.Departman = "Yazılım";
+
+            calisan2.CalisanBilgileri();
+
+        }
+        class Calisan
+        {
+            public string Ad;
+            public string Soyad;
+            public int No;
+            public string Departman;
+
+            public void CalisanBilgileri()
+            {
+                Console.WriteLine("Çalışan Adı:{0}", Ad);
+                Console.WriteLine("Çalışan Soyadı:{0}", Soyad);
+                Console.WriteLine("Çalışan Numarası:{0}", No);
+                Console.WriteLine("Çalışan Departmanı:{0}", Departman);
+            }
+
+        }
+
+    }
+}
+
---
+++ dictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //system collections generic
+           Dictionary<int, string> kullanıcılar = new Dictionary<int, string>();
+            kullanıcılar.Add(10, "Ayşe Yılmaz");
+            kullanıcılar.Add(12, "Ahmet Yıldırım");
+            kullanıcılar.Add(18, "Deniz Arda");
+            kullanıcılar.Add(31, "Özcan Coşar");
+
+            //Dizinin elemanlarına erişim
+            Console.WriteLine("****Elemanlara erişim****");
+            Console.WriteLine(kullanıcılar[12]);
+
+            foreach(var item in kullanıcılar)
+                Console.WriteLine(item);
+
+            //COunt;
+            Console.WriteLine(kullanıcılar.Count);
+
+            //Contains;
+            Console.WriteLine(kullanıcılar.ContainsKey(12));
+            Console.WriteLine(kullanıcılar.ContainsValue("Zikriye"));
+
+            //remove
+            kullanıcılar.Remove(12);
+            foreach (var item in kullanıcılar)
+                Console.WriteLine(item.Value);
+
+            //keys & values
+            Console.WriteLine("Keys");
+            foreach (var item in kullanıcılar.Keys)
+                Console.WriteLine(item);
+
+            Console.WriteLine("Valuess");
+            foreach (var item in kullanıcılar.Values)
+                Console.WriteLine(item);
+        }
+    }
+}
