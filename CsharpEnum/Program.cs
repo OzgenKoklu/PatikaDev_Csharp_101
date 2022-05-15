f776d045d141c87813c2b069864734c686c5eb2a
Ozgen Koklu <ozgen.koklu@gmail.com>
2022-05-15 10:45:57 +0300

    Enum notları eklendi

---
+++ CsharpEnum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CsharpEnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Pazartesi);
+
+            int sicaklik = 32; 
+
+            if(sicaklik <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            else if(sicaklik >= (int)HavaDurumu.Sicak)
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün.");
+            else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik <= (int)HavaDurumu.Sicak)
+                Console.WriteLine("Hadi dışarıya çıkalım.");
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma=23, //cumartesi ve pazar 24 ve 25 olacak
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 35
+    }
+}
---
+++ structConcept/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace structConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //classlar default constuctor kullanmasak bile initial değer olarak atama yapabilir
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar = 3;
+            dikdortgen.UzunKenar = 4;
+
+            Console.WriteLine("Class alan hesabı: {0}", dikdortgen.AlanHesapla());
+
+            Dikdortgen_Struct dikdortgen_struct; //= new Dikdortgen_Struct(); structlarda bunu yazmak zorunda değilsiniz.
+            dikdortgen_struct.KisaKenar = 3;
+            dikdortgen_struct.UzunKenar = 4;
+            //structlarda değer vermezsek initial değer veremez. stuctlar heap'de değil stackte tutulduğu için performans artışı sağlayabilir. 
+            //16 byte'a kadar olan verileriniz için stack, 16+ byte için class daha mantıklı, referans tipinin gücünün avantajı daha fazla.
+            Console.WriteLine("Struct alan hesabı: {0}", dikdortgen_struct.AlanHesapla());
+
+        }
+    
+
+    }
+
+    class Dikdortgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        //default Constuctor'a izin var
+        public Dikdortgen()
+        {
+            KisaKenar = 2;
+            UzunKenar = 3;
+        }
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+
+
+    struct Dikdortgen_Struct
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        //default Constuctor'a izin yok.
+
+        public Dikdortgen_Struct(int kisaKenar, int uzunKenar)
+        {
+            KisaKenar = kisaKenar;
+            UzunKenar = uzunKenar;
+        }
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+
+}
